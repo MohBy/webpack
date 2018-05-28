01f92a351b10a77f3813956db26d0b79ade86f93
ganndal <mohbarry1986@gmail.com>
2018-05-28 13:43:52 +0200

    first

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import {changeColor} from "./homepage";
+
+
+console.log("This is complicated");
+
+changeColor("h1", "red");
---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require ("path")
+module.exports = {
+    // The main file for our js
+    entry: "./src/index.js" ,
+    // The output files
+    output: {
+        // Where we want to put our output files
+        path: path.resolve(__dirname, "dist"),
+        // The name for our main file
+        filename: "bundle.js"
+    }
+}
